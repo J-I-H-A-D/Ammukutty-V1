e0deacdb59bbac45be115653c206e7a30aba486c
JiHaD SeR <85656190+J-I-H-A-D@users.noreply.github.com>
2021-08-17 16:46:03 +0530

    Update _role.js

--- plugins/_role.js
+++ plugins/_role.js
@@ -3,13 +3,13 @@ let handler = m => m
 handler.before = function (m) {
   let user = global.db.data.users[m.sender]
         let role = (user.level <= 10) ? 'Beginner'
-          : ((user.level >= 10) && (user.level <= 20)) ? 'Kittel Town'
-          : ((user.level >= 20) && (user.level <= 30)) ? 'Black Woods'
-          : ((user.level >= 30) && (user.level <= 40)) ? 'Farmount'
-          : ((user.level >= 40) && (user.level <= 50)) ? 'Rosenvale'
-          : ((user.level >= 50) && (user.level <= 60)) ? 'Amberhill'
-          : ((user.level >= 60) && (user.level <= 70)) ? 'Master Fap'
-          : ((user.level >= 70) && (user.level <= 80)) ? 'Master Woods'
+          : ((user.level >= 10) && (user.level <= 20)) ? 'Bronze🥉'
+          : ((user.level >= 20) && (user.level <= 30)) ? 'Silver🥈'
+          : ((user.level >= 30) && (user.level <= 40)) ? 'Gold🥇'
+          : ((user.level >= 40) && (user.level <= 50)) ? 'Platinum😎'
+          : ((user.level >= 50) && (user.level <= 60)) ? 'Diamond🌝'
+          : ((user.level >= 60) && (user.level <= 70)) ? 'Crown😌'
+          : ((user.level >= 70) && (user.level <= 80)) ? 'Conqueror🔱'
           : 'Legend'
   user.role = role
   return true
