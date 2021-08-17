aa5efc4546aa32b1bdcf67907592f660b95e6a24
JiHaD SeR <85656190+J-I-H-A-D@users.noreply.github.com>
2021-08-17 17:29:17 +0530

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,7 +1,7 @@
 function handler(m) {
   // Ini cuma contoh, jgn di uncomment -_-
-  // F this.sendContact(m.chat, '62815158600891', 'Nurutomo', m)
-  this.sendContact(m.chat, '6281334177340', 'Nurutomo', m)
+  // F this.sendContact(m.chat, '917736703116', 'I am Jihad', m)
+  this.sendContact(m.chat, '917736703116', 'JiHaD', m)
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
