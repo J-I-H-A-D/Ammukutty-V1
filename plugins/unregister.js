8182cd8c44c8a931f2a9472e616dd6b3e09561ca
JiHaD SeR <85656190+J-I-H-A-D@users.noreply.github.com>
2021-08-17 17:26:11 +0530

    Update unregister.js

--- plugins/unregister.js
+++ plugins/unregister.js
@@ -3,9 +3,9 @@ let handler = async function (m, { args }) {
   if (!args[0]) throw 'Serial Number kosong'
   let user = global.db.data.users[m.sender]
   let sn = createHash('md5').update(m.sender).digest('hex')
-  if (args[0] !== sn) throw 'Serial Number salah'
+  if (args[0] !== sn) throw 'Wrong serial number'
   user.registered = false
-  m.reply(`Unreg berhasil!`)
+  m.reply(`Unreg did it!`)
 }
 handler.help = ['', 'ister'].map(v => 'unreg' + v + ' <SN|SERIAL NUMBER>')
 handler.tags = ['exp']
